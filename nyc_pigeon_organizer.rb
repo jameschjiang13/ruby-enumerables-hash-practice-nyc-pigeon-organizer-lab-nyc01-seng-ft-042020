b443bde29286ab8378a50864aae2551d4f3f584e
James Jiang <monochrome473@gmail.com>
2020-04-17 21:15:11 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
+  name_array = data[:gender][:male].concat(data[:gender][:female])
+  pigeon_list = {}
+  counter = 0
+  while counter < name_array.length do
+    binding.pry
+    pigeon_list{
+      :name_array[counter] => {
+        :color =>[],
+        :gender =>[],
+        :lives =>[]
+      }
+    }
+    counter += 1
+  end
+  binding.pry
 end
