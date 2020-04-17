b07a284275259e82d9cd84065c8cae86c1c31c69
James Jiang <monochrome473@gmail.com>
2020-04-17 21:54:04 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -15,36 +15,16 @@ def nyc_pigeon_organizer(data)
     pigeon_list.merge!(new_member)
     counter += 1
   end
-binding.pry
-end
-=begin
-pigeon_data = {
-  :color => {
-    :purple => ["Theo", "Peter Jr.", "Lucky"],
-    :grey => ["Theo", "Peter Jr.", "Ms. K"],
-    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
-    :brown => ["Queenie", "Alex"]
-  },
-  :gender => {
-    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
-    :female => ["Queenie", "Ms. K"]
-  },
-  :lives => {
-    "Subway" => ["Theo", "Queenie"],
-    "Central Park" => ["Alex", "Ms. K", "Lucky"],
-    "Library" => ["Peter Jr."],
-    "City Hall" => ["Andrew"]
-  }
-}
 
   counter_color = 0
-  while counter_color < pigeon_data[:color].length do
+  while counter_color < data[:color].length do
     if pigeon_data[:color].values[counter_color].include? "name_array[counter]"
       pigeon_list[name_array[counter]][color] << pigeon_data[:color].keys[counter_color]
     end
     counter_color += 1
   end
-
+binding.pry
+=begin
   counter_gender = 0
   while counter_gender < pigeon_data[:gender].length do
     if pigeon_data[:gender].values[counter_gender].include? "name_array[counter]"
@@ -60,4 +40,4 @@ pigeon_data = {
     end
     counter_gender += 1
   end
-=end
+end
