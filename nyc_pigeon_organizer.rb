c4300b5732aa2be7e98613a195dad813dfd0644b
James Jiang <monochrome473@gmail.com>
2020-04-17 21:56:51 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -15,6 +15,7 @@ def nyc_pigeon_organizer(data)
     pigeon_list.merge!(new_member)
     counter += 1
   end
+  binding.pry
 
   counter_color = 0
   while counter_color < data[:color].length do
@@ -23,7 +24,6 @@ def nyc_pigeon_organizer(data)
     end
     counter_color += 1
   end
-binding.pry
 
 =begin
   counter_gender = 0
